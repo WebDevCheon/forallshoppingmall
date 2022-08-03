d45541286e65d0d52b749d83a84a3fa9fc3dac74
WebDevCheon <chunseunghun@naver.com>
2022-08-03 16:56:11 +0900

    getShoppingBasket,checkCartSpace Test Code Updated

--- src/test/java/spring/myapp/shoppingmall/test/AppServiceTest.java
+++ src/test/java/spring/myapp/shoppingmall/test/AppServiceTest.java
@@ -1,29 +1,24 @@
 package spring.myapp.shoppingmall.test;
 
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.atMost;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
+import org.springframework.transaction.annotation.Transactional;
 import spring.myapp.shoppingmall.dao.MallDao;
 import spring.myapp.shoppingmall.dto.Shoppingbasket;
-import spring.myapp.shoppingmall.dto.User;
 import spring.myapp.shoppingmall.service.ShoppingBasketImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -35,7 +30,6 @@ import spring.myapp.shoppingmall.service.ShoppingBasketImpl;
 @WebAppConfiguration
 public class AppServiceTest {
 	
-	private static final Logger logger = LoggerFactory.getLogger(AppServiceTest.class);
 	private ShoppingBasketImpl shoppingBasketImpl;
 	@Mock
 	private MallDao mallDao;
@@ -48,6 +42,7 @@ public class AppServiceTest {
 	}
 	
 	@Test
+	@Transactional
 	@DisplayName("setShoppingBasket 테스트")
 	public void setShoppingBasketTest() {
 		int qty = 8; int goods_id = 34; int price = 100; String User_ID = "admin3"; String name = "후가는 유가";
@@ -59,29 +54,20 @@ public class AppServiceTest {
 	@Test
 	@DisplayName("getShoppingBasket 테스트")
 	public void getShoppingBasketListTest(){
-		Shoppingbasket b1 = new Shoppingbasket();
-		Shoppingbasket b2 = new Shoppingbasket();
-		Shoppingbasket b3 = new Shoppingbasket();
-		List<Shoppingbasket> testResultList = new ArrayList<Shoppingbasket>();
-		testResultList.add(b1); 
-		testResultList.add(b2); 
-		testResultList.add(b3);
-		when(mallDao.getShoppingbasket("admin")).thenReturn(testResultList);
-		List<Shoppingbasket> result = shoppingBasketImpl.getShoppingBasketList("admin");
-		assertTrue(result.get(0) == b1);
-		assertTrue(result.get(1) == b2);
-		assertTrue(result.get(2) == b3);
-		verify(mallDao).getShoppingbasket("admin");
+		String UserId = "admin";
+		when(mallDao.getShoppingbasket(UserId)).thenReturn(new ArrayList<Shoppingbasket>());
+		List<Shoppingbasket> cartList = shoppingBasketImpl.getShoppingBasketList(UserId);
+		assertEquals(cartList.size(),0);
+		verify(mallDao).getShoppingbasket(UserId);
 	}
 	
 	@Test
 	@DisplayName("checkCartSpace 테스트")
 	public void checkCartSpaceTest() {
-		User user = new User();
-		user.setCarts(new ArrayList<Shoppingbasket>());
-		user.setCarts(new ArrayList<Shoppingbasket>());
-		user.setCarts(new ArrayList<Shoppingbasket>());
-		when(shoppingBasketImpl.checkCartSpace("admin")).thenReturn(3);
-		assertTrue(shoppingBasketImpl.checkCartSpace("admin") == 3);
+		String UserId = "admin";
+		when(mallDao.cartspace(UserId)).thenReturn(0);
+		int cartSpace = shoppingBasketImpl.checkCartSpace(UserId);
+		assertEquals(cartSpace,0);
+		verify(mallDao).cartspace(UserId);
 	}
 }
